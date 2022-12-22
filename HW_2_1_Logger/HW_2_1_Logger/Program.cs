ae956108d44a39f583ffcc361374fc1ffe35ec2a
MiNeris0 <seikolor@gmail.com>
2022-12-22 20:00:57 +0200

    Add work file.

---
+++ HW_2_1_Logger/HW_2_1_Logger/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HW_2_1_Logger
+{
+    /// <summary>
+    /// start point of Logger program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Start of app.
+        /// </summary>
+        /// <param name="args">args added via console. </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 22; i++)
+            {
+                int o = 0;
+                Console.Write(o);
+            }
+        }
+    }
+}
